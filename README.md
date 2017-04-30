01cf8e1c9a595f294a331e66b1b8832120e7ac1e
Max Choi <hanpama@gmail.com>
2017-04-30 14:05:05 +0900

    antd layout

---
+++ README.md
@@ -0,0 +1,8 @@
+# Next With Antd
+
+A simple example for showing how to initialize your frontend project with next.js and antd.
+
+![screenshot](screenshot.png)
+
+- [Including plain css files in Next.js project](https://github.com/zeit/next.js/tree/master/examples/with-global-stylesheet)
+- [Ant.design layout example](https://ant.design/components/layout/)
--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,78 @@
 import * as React from 'react';
 import stylesheet from '../node_modules/antd/dist/antd.css'
 
-import { Button, Radio, Icon } from 'antd';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 
-class ButtonSize extends React.Component {
-  state = {
-    size: 'default',
-  };
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
-  handleSizeChange = (e) => {
-    this.setState({ size: e.target.value });
-  }
-
+export default class App extends React.Component {
   render() {
-    const size = this.state.size;
-    return (
-      <div>
+    return <Layout>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+      <style jsx>{`
+        #components-layout-demo-top-side-2 .logo {
+          width: 120px;
+          height: 31px;
+          background: #333;
+          border-radius: 6px;
+          margin: 16px 28px 16px 0;
+          float: left;
+        }
+      `}</style>
 
-        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-
-        <Radio.Group value={size} onChange={this.handleSizeChange}>
-          <Radio.Button value="large">Large</Radio.Button>
-          <Radio.Button value="default">Default</Radio.Button>
-          <Radio.Button value="small">Small</Radio.Button>
-        </Radio.Group>
-        <br /><br />
-        <Button type="primary" shape="circle" icon="download" size={size} />
-        <Button type="primary" icon="download" size={size}>Download</Button>
-        <Button type="primary" size={size}>Normal</Button>
-        <br />
-        <Button.Group size={size}>
-          <Button type="primary">
-            <Icon type="left" />Backward
-          </Button>
-          <Button type="primary">
-            Forward<Icon type="right" />
-          </Button>
-        </Button.Group>
-      </div>
-    );
-  }
-}
-
-export default class Home extends React.Component {
-  render() {
-    return <div>
-      <h1>Hi!</h1>
-      <ButtonSize />
-    </div>;
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%' }}
+          >
+            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '12px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   }
 }
